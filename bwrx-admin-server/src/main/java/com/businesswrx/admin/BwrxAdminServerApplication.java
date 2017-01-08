6739159e96d90abe6220d8f57f2c1d250239f01f
Patty Eilers <patricia.eilers@gmail.com>
2017-01-08 07:01:21 -0700

    initial GS work for spring boot

---
+++ bwrx-admin-server/src/main/java/com/businesswrx/admin/BwrxAdminServerApplication.java
@@ -0,0 +1,12 @@
+package com.businesswrx.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwrxAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BwrxAdminServerApplication.class, args);
+	}
+}
---
+++ bwrx-admin-server/src/test/java/com/businesswrx/admin/BwrxAdminServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.businesswrx.admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BwrxAdminServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
