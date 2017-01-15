29ef97d3cd5d03cb8c2ae94deaf5f2bf1f0d57de
Patty Eilers <patricia.eilers@gmail.com>
2017-01-15 06:54:02 -0700

    REST controller prototyping

--- bwrx-admin-server/src/main/java/com/businesswrx/admin/BwrxAdminServerApplication.java
+++ bwrx-admin-server/src/main/java/com/businesswrx/admin/BwrxAdminServerApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -28,5 +32,16 @@ public class BwrxAdminServerApplication {
 			
 		};
 	}
+	
+	//CORS web service
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:9000");
+            }
+        };
+    }
 	
 }
